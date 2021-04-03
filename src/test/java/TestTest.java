5084cf96416af8994a7991a1b1490369e85873f4
Daniel Delegacz <ddelegacz@hotmail.com>
2021-04-03 17:39:01 +0200

    Initialization

---
+++ src/test/java/TestTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class TestTest {
+
+    @Test
+    void example(){
+        fail();
+    }
+
+
+
+}
