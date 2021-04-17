7394d7f0817dca8572724fbeb2a325c6e6d9dbf4
Daniel Delegacz <ddelegacz@gmail.com>
2021-04-17 22:09:30 +0200

    add creature to not empty field test

--- src/test/java/pl/dele/BoardTest.java
+++ src/test/java/pl/dele/BoardTest.java
@@ -39,4 +39,20 @@ class BoardTest {
 
         assertNull(creatureFromBoard);
     }
+
+    @Test
+    void shouldXWhenYouTryAddCreatureToNotEmptyField(){
+        // add create to 0,0 field
+        board.add(new Point(0,0), creature);
+
+        // try to add another creature to 0,0 field
+        Creature creature2 = new Creature();
+        board.add(new Point(0,0), creature2);
+
+        // get creature from board
+        Creature creatureFromBoard = board.get(0,0);
+
+        // compare creatures
+        assertEquals(creature, creatureFromBoard);
+    }
 }
