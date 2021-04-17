3985c670665d7c3209a6b56986587d97bb014a1c
Daniel Delegacz <ddelegacz@gmail.com>
2021-04-17 21:27:49 +0200

    test commit

---
+++ src/test/java/pl/dele/ExampleTest.java
@@ -0,0 +1,13 @@
+package pl.dele;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ExampleTest {
+
+    @Test
+    public void example(){
+        assertEquals(2,1+1);
+    }
+}
