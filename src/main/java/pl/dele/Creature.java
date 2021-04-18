f491d48bc4ff6340cc9b924b34f52bfb150d03d4
Daniel Delegacz <ddelegacz@gmail.com>
2021-04-18 13:00:28 +0200

    Add stats to Creature

--- src/main/java/pl/dele/Creature.java
+++ src/main/java/pl/dele/Creature.java
@@ -1,4 +1,17 @@
 package pl.dele;
 
 public class Creature {
+
+    // base statistics of creature
+    private final CreatureStatistics stats;
+    private int currentHp;
+
+    Creature(){
+        stats = new CreatureStatistics("DefName", 0,
+                0, 10, 100);
+    }
+
+    Creature(String name, int attack, int armor, int maxHp, int moveRange) {
+        stats = new CreatureStatistics(name, attack, armor, maxHp, moveRange);
+    }
 }
