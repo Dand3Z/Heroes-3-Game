d06e76d8f68ad78865700a2d71f10f4a13935190
Daniel Delegacz <ddelegacz@gmail.com>
2021-04-18 16:31:00 +0200

    initial game engine

---
+++ src/main/java/pl/dele/GameEngine.java
@@ -0,0 +1,37 @@
+package pl.dele;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameEngine {
+
+    private final Board board;
+    private final CreatureTurnQueue queue;
+
+    // init game
+    GameEngine(List<Creature> creatures1, List<Creature> creatures2){
+        board = new Board();
+        putCreatureToBoard(creatures1, creatures2);
+        // create new list with creatures1 and creatures2
+        List<Creature> twoSidesCreatures = new ArrayList<>();
+        twoSidesCreatures.addAll(creatures1);
+        twoSidesCreatures.addAll(creatures2);
+        queue = new CreatureTurnQueue(twoSidesCreatures);
+    }
+
+    // engine knows active creature
+    void move(Point targetPoint){
+        board.move(targetPoint, queue.getActiveCreature());
+    }
+
+    private void putCreatureToBoard(List<Creature> creatures1, List<Creature> creatures2) {
+        putCreaturesFromOneSideOfBoard(creatures1,0);
+        putCreaturesFromOneSideOfBoard(creatures2,board.WIDTH-1);
+    }
+
+    private void putCreaturesFromOneSideOfBoard(List<Creature> creatures1, int x) {
+        for (int i = 0; i < creatures1.size(); ++i){
+            board.add(new Point(x,i * 2), creatures1.get(i));
+        }
+    }
+}
