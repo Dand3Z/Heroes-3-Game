c1166008bf9fdb1323955450295e592c327aac25
Daniel Delegacz <ddelegacz@gmail.com>
2021-04-18 15:02:52 +0200

    add Creature attack implementation and more tests

--- src/main/java/pl/dele/Creature.java
+++ src/main/java/pl/dele/Creature.java
@@ -1,5 +1,7 @@
 package pl.dele;
 
+import java.security.InvalidParameterException;
+
 public class Creature {
 
     // base statistics of creature
@@ -7,15 +9,28 @@ public class Creature {
     private int currentHp;
 
     Creature(){
-        stats = new CreatureStatistics("DefName", 0,
-                0, 10, 100);
+        this("DefName", 0, 0, 10, 100);
     }
 
     Creature(String name, int attack, int armor, int maxHp, int moveRange) {
         stats = new CreatureStatistics(name, attack, armor, maxHp, moveRange);
+        currentHp = stats.getMaxHp();
     }
 
     int getCurrentHp() {
         return currentHp;
+    }
+
+    void attack(Creature defender) {
+        if (isAlive()){
+            if (defender == null) throw new InvalidParameterException();
+            var attackValue = this.stats.getAttack() - defender.stats.getArmor();
+
+            if (attackValue > 0) defender.currentHp -= attackValue;
+        }
+    }
+
+    private boolean isAlive(){
+        return currentHp > 0;
     }
 }
