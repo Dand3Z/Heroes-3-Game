21e962b9bba19572bbf7dfb82eea4df69013dbae
Daniel Delegacz <ddelegacz@gmail.com>
2021-04-18 12:30:46 +0200

    queue first implementation

--- src/main/java/pl/dele/CreatureTurnQueue.java
+++ src/main/java/pl/dele/CreatureTurnQueue.java
@@ -1,13 +1,29 @@
 package pl.dele;
 
 import java.util.Collection;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class CreatureTurnQueue {
 
 
     private final Collection<Creature> creatures;
+    private final Queue<Creature> creaturesQueue;
+    private Creature activeCreature;
 
     public CreatureTurnQueue(Collection<Creature> creatureList) {
         creatures = creatureList;
+        creaturesQueue = new LinkedList<>();
+        creaturesQueue.addAll(creatures);
+        // set first activeCreature
+        next();
+    }
+
+    Creature getActiveCreature() {
+        return activeCreature;
+    }
+
+    void next() {
+        activeCreature = creaturesQueue.poll();
     }
 }
