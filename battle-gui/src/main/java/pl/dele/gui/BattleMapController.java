6361d6b1793693e39d41d210c5a548b465e9d01b
Daniel Delegacz <ddelegacz@gmail.com>
2021-05-19 21:23:41 +0200

    first grid

--- battle-gui/src/main/java/pl/dele/gui/BattleMapController.java
+++ battle-gui/src/main/java/pl/dele/gui/BattleMapController.java
@@ -1,5 +1,22 @@
 package pl.dele.gui;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
 public class BattleMapController {
 
+    @FXML
+    private GridPane gripMap;
+
+    @FXML
+    void initialize(){
+        for(int x = 0; x < 20; ++x)
+            for(int y = 0; y < 15; ++y){
+                Rectangle rectangle = new Rectangle(42,42, Color.WHITE);
+                rectangle.setStroke(Color.BLACK);
+                gripMap.add(rectangle,x,y);
+            }
+    }
 }
